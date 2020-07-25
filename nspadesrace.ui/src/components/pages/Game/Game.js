15f67757cca167cad03b2e719472412e37ae3ce1
Ivan Phelps <ivanniothemannio@gmail.com>
2020-07-25 16:35:49 -0500

    squashed bugs, finalized game screen

--- nspadesrace.ui/src/components/pages/Game/Game.js
+++ nspadesrace.ui/src/components/pages/Game/Game.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Page, Col, Row, Button, Dialog, Toast, BottomToolbar, ProgressCircular, Icon } from "react-onsenui";
+import { Link } from "react-router-dom";
+import {
+  Page,
+  Col,
+  Row,
+  Button,
+  AlertDialog,
+  AlertDialogButton,
+  ProgressCircular,
+  Toast,
+  BottomToolbar,
+  Icon,
+} from "react-onsenui";
+import ParticlesBg from "particles-bg";
 import getCards from "../../../helpers/data/getCards";
 import Card from "../../shared/Card/Card";
-import playerData from "../../../helpers/data/playerData.js";
-import scoreData from '../../../helpers/data/scoreData.js';
+import scoreData from "../../../helpers/data/scoreData.js";
 import "onsenui/css/onsenui.css";
 import "onsenui/css/onsen-css-components.css";
 import "./Game.scss";
@@ -15,48 +26,41 @@ class Game extends React.Component {
     timerOn: false,
     timerStart: 0,
     timerTime: 0,
-    player: null,
     stack: [],
     nonMatches: [],
     selectedCard: {
-      value: 'first',
+      value: "first",
     },
     matches: 0,
     highScore: null,
     toastOpen: false,
+    toastText: "",
+    rulesOpen: false,
+    gameFinished: false,
   };
 
   componentDidMount() {
-    const { authed } = this.props;
+    const { authed, player } = this.props;
     if (authed) {
-      this.getPlayer(this.props.firebaseUid);
-    };
-    this.getCards();  
+      this.getHighScore(player);
+    }
+    this.getCards();
+    this.setState({ loading: false });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.props !== prevProps) {
-      if (this.props.firebaseUid !== '') {
-        this.getPlayer(this.props.firebaseUid);
-      };   
-    };
-    if (this.state.player !== prevState.player) {
-      this.getHighScore(this.state.player);
-    }    
+    if (this.props.player !== prevProps.player) {
+      if (this.props.player.id !== undefined)
+        this.getHighScore(this.props.player);
+    }
   }
 
-  getPlayer = (uid) => {
-    playerData
-      .getPlayerByFirebaseUid(uid)
-      .then((response) => this.setState({ player: response, loading: false }))
-      .catch((error) => console.error("error getting user", error));
-  };
-
   getHighScore = (player) => {
-    scoreData.getHighestScoreByPlayerId(player.id)
-    .then((response) => this.setState({ highScore: response }))
-    .catch((error) => console.error("error getting high score", error));
-  }  
+    scoreData
+      .getHighestScoreByPlayerId(player.id)
+      .then((response) => this.setState({ highScore: response }))
+      .catch((error) => console.error("error getting high score", error));
+  };
 
   getCards = () => {
     const stack = getCards.getCards();
@@ -83,7 +87,7 @@ class Game extends React.Component {
 
   setSelectedCard = (card) => {
     this.setState({ selectedCard: card });
-  }
+  };
 
   clearSelectedCard = () => {
     this.setState({
@@ -91,12 +95,12 @@ class Game extends React.Component {
         value: "cleared",
       },
     });
-  }
+  };
 
   handleNoMatch = (cardOne, cardTwo) => {
     this.clearSelectedCard();
-    this.setState({ nonMatches: [cardOne, cardTwo]});
-  }
+    this.setState({ nonMatches: [cardOne, cardTwo] });
+  };
 
   handleMatch = (cardOne, cardTwo) => {
     let { matches } = this.state;
@@ -106,45 +110,96 @@ class Game extends React.Component {
       this.finish();
     }
     this.clearSelectedCard();
-  }
+  };
 
   addScore = (score) => {
-    scoreData.addScore(score)
-    .then((response) => console.log("added score:", response))
-    .catch((error) => console.error("error adding score to db:", error))
-  }  
+    scoreData
+      .addScore(score)
+      .then((response) => console.log("added score:", response))
+      .catch((error) => console.error("error adding score to db:", error));
+  };
 
   newHighScore = (score) => {
-    this.setState({ toastOpen: true, highScore: score  })
-  }
+    this.setState({ highScore: score });
+    this.toaster("Nice! You just got a new high score!");
+  };
+
+  firstWin = (score) => {
+    this.setState({ highScore: score });
+    this.toaster("Congrats! you won your first game!");
+  };
+
+  toaster = (text) => {
+    this.setState({ toastOpen: true, toastText: text });
+    setTimeout(() => {
+      this.setState({ toastOpen: false, toastText: "" });
+    }, 4000);
+  };
 
   finish = () => {
     this.stopTimer();
+    this.setState({ gameFinished: true });
     if (this.props.authed) {
       const { timerTime, highScore } = this.state;
       let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
       let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
       let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
       const scoreToAdd = {
-        playerId: this.state.player.id,
+        playerId: this.props.player.id,
         raw: timerTime,
         time: `${minutes}:${seconds}:${centiseconds}`,
-      }
-      this.addScore(scoreToAdd); 
-      if (highScore.raw > scoreToAdd.raw) { 
+      };
+      this.addScore(scoreToAdd);
+      if (highScore === null) {
+        this.firstWin(scoreToAdd);
+      } else if (highScore.raw > scoreToAdd.raw) {
         this.newHighScore(scoreToAdd);
       }
       console.log(scoreToAdd);
     }
-  }
+  };
 
   openRules = () => {
-    // console.log();
-  }
+    this.setState({ rulesOpen: true });
+  };
+
+  resetGame = () => {
+    this.stopTimer();
+    this.setState({
+      gameFinished: false,
+      loading: true,
+      timerStart: 0,
+      timerTime: 0,
+      stack: [],
+      selectedCard: {
+        value: "first",
+      },
+      matches: 0,
+    });
+    setTimeout(() => {
+      this.getCards();
+      this.setState({ loading: false });
+    }, 1000);
+  };
+
+  closeRules = () => {
+    this.setState({ rulesOpen: false });
+  };
 
   render() {
-    const { timerOn, timerTime, stack, selectedCard, matches, nonMatches, toastOpen, player, highestScore, loading } = this.state;
-    const { authed } = this.props;
+    const {
+      loading,
+      timerOn,
+      timerTime,
+      stack,
+      selectedCard,
+      matches,
+      nonMatches,
+      toastOpen,
+      rulesOpen,
+      gameFinished,
+    } = this.state;
+    const { authed, player } = this.props;
     const rowOne = stack.slice(0, 6);
     const rowTwo = stack.slice(6, 12);
     const rowThree = stack.slice(12, 18);
@@ -162,118 +217,197 @@ class Game extends React.Component {
             {minutes}:{seconds}:{centiseconds}
           </div>
         </Col>
-        <Row className="card-row">
-          {rowOne.map((card) => (
-            <Card
-              nonMatches={nonMatches}
-              matches={matches}
-              handleMatch={this.handleMatch}
-              handleNoMatch={this.handleNoMatch}
-              clearSelectedCard={this.clearSelectedCard}
-              startTimer={this.startTimer}
-              setSelectedCard={this.setSelectedCard}
-              selectedCard={selectedCard}
-              key={card.id}
-              card={card}
-            ></Card>
-          ))}
-        </Row>
-        <Row className="card-row">
-          {rowTwo.map((card) => (
-            <Card
-            nonMatches={nonMatches}
-            matches={matches}
-            handleMatch={this.handleMatch}
-            handleNoMatch={this.handleNoMatch}
-            clearSelectedCard={this.clearSelectedCard}
-            startTimer={this.startTimer}
-            setSelectedCard={this.setSelectedCard}
-            selectedCard={selectedCard}
-            key={card.id}
-            card={card}
-            ></Card>
-          ))}
-        </Row>
-        <Row className="card-row">
-          {rowThree.map((card) => (
-            <Card
-            nonMatches={nonMatches}
-            matches={matches}
-            handleMatch={this.handleMatch}
-            handleNoMatch={this.handleNoMatch}
-            clearSelectedCard={this.clearSelectedCard}
-            startTimer={this.startTimer}
-            setSelectedCard={this.setSelectedCard}
-            selectedCard={selectedCard}
-            key={card.id}
-            card={card}
-            ></Card>
-          ))}
-        </Row>
-        <Row className="card-row">
-          {rowFour.map((card) => (
-            <Card
-            nonMatches={nonMatches}
-            matches={matches}
-            handleMatch={this.handleMatch}
-            handleNoMatch={this.handleNoMatch}
-            clearSelectedCard={this.clearSelectedCard}
-            startTimer={this.startTimer}
-            setSelectedCard={this.setSelectedCard}
-            selectedCard={selectedCard}
-            key={card.id}
-            card={card}
-            ></Card>
-          ))}
-        </Row>
-        <Row className="card-row">
-          {rowFive.map((card) => (
-            <Card
-            nonMatches={nonMatches}
-            matches={matches}
-            handleMatch={this.handleMatch}
-            handleNoMatch={this.handleNoMatch}
-            clearSelectedCard={this.clearSelectedCard}
-            startTimer={this.startTimer}
-            setSelectedCard={this.setSelectedCard}
-            selectedCard={selectedCard}
-            key={card.id}
-            card={card}
-            ></Card>
-          ))}
-        </Row>
-        <Row className="card-row">
-          {rowSix.map((card) => (
-            <Card
-            nonMatches={nonMatches}
-            matches={matches}
-            handleMatch={this.handleMatch}
-            handleNoMatch={this.handleNoMatch}
-            clearSelectedCard={this.clearSelectedCard}
-            startTimer={this.startTimer}
-            setSelectedCard={this.setSelectedCard}
-            selectedCard={selectedCard}
-            key={card.id}
-            card={card}
-            ></Card>
-          ))}
-        </Row>
-        <BottomToolbar className='game-bar'>
-          { loading ? <ProgressCircular indeterminate /> : <><div className="game-bar-left">
-          { player !== null ? <div className="icon-auth-container"><Icon className="logged-in" size={30} icon='fa-check-circle'></Icon>logged in as: <b>{player.userName}</b></div> : <div className="icon-auth-container"><Icon className="logged-out" size={30} icon='fa-times-circle'></Icon>Logged out</div>}
-          </div> 
+        {loading ? (
+          <Col className="loader-container">
+            <ProgressCircular
+              className="loading-circle"
+              modifier="material"
+              indeterminate
+            />
+          </Col>
+        ) : (
+          <>
+            <Row className="card-row">
+              {rowOne.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+            <Row className="card-row">
+              {rowTwo.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+            <Row className="card-row">
+              {rowThree.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+            <Row className="card-row">
+              {rowFour.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+            <Row className="card-row">
+              {rowFive.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+            <Row className="card-row">
+              {rowSix.map((card) => (
+                <Card
+                  nonMatches={nonMatches}
+                  matches={matches}
+                  handleMatch={this.handleMatch}
+                  handleNoMatch={this.handleNoMatch}
+                  clearSelectedCard={this.clearSelectedCard}
+                  startTimer={this.startTimer}
+                  setSelectedCard={this.setSelectedCard}
+                  selectedCard={selectedCard}
+                  key={card.id}
+                  card={card}
+                ></Card>
+              ))}
+            </Row>
+          </>
+        )}
+
+        <BottomToolbar modifier="material" className="game-bar">
+          {authed ? (
+            <div className="game-bar-left">
+              <div className="icon-auth-container">
+                <Icon
+                  className="logged-in"
+                  size={30}
+                  icon="fa-check-circle"
+                ></Icon>
+                logged in as: <b>{player.userName}</b>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+
           <div className="game-bar-right">
-            { timerOn ? <Button modifier="material" className="reset-button">
-              Reset
-            </Button> : <><Button onClick={this.openRules} modifier='material' className="custom-button">
-              How To Play
-            </Button>
-            <Link className="custom-button" to={"/sign-up"}>
-              <Button className="custom-button">Login / Create Account</Button>
-            </Link></>}          
-          </div></> }     
+            {timerOn || gameFinished ? (
+              <Button
+                onClick={this.resetGame}
+                modifier="material"
+                className="reset-button"
+              >
+                Reset
+              </Button>
+            ) : (
+              <>
+                <Button
+                  onClick={this.openRules}
+                  modifier="material"
+                  className="custom-button"
+                >
+                  How To Play
+                </Button>
+                {authed ? (
+                  <Link className="custom-button" to={"/"}>
+                    <Button className="custom-button" modifier="material">
+                      Home
+                    </Button>
+                  </Link>
+                ) : (
+                  <Link className="custom-button" to={"/sign-up"}>
+                    <Button className="custom-button" modifier="material">
+                      Login / Create Account
+                    </Button>
+                  </Link>
+                )}
+              </>
+            )}
+          </div>
         </BottomToolbar>
-        <Toast modifier='material' isOpen={toastOpen}></Toast> 
+        { rulesOpen ? <AlertDialog
+          isOpen={true}
+          onCancel={this.closeRules}
+          cancelable
+        >
+          <div className="alert-dialog-title">How To Play</div>
+          <div className="alert-dialog-content">
+            -Tap a card to flip it over and reveal the face value.
+            <br />
+            -Time starts as soon as the first card's face is shown.
+            <br />
+            -Flip over another card. If it is a match, the cards will remain
+            flipped.
+            <br />
+            -If the cards do not match, thy will flip back over.
+            <br />
+            -Try to remember which cards are where, and match them all as fast
+            as possible to win!
+          </div>
+          <div className="alert-dialog-footer">
+            <AlertDialogButton
+              onClick={this.closeRules}
+              className="alert-dialog-button"
+            >
+              OK!
+            </AlertDialogButton>
+          </div>
+        </AlertDialog> : <></> }
+        
+        <Toast isOpen={toastOpen}> {this.state.toastText} </Toast>
+        {gameFinished ? <ParticlesBg type="lines" bg={true} /> : <></>}
       </Page>
     );
   }
